49240564efd65c829bee27e161b49c1a3fcb0b78
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-04-15 18:32:31 +0100

    [ cleanup ]

--- .github/scripts/katla.sh
+++ .github/scripts/katla.sh
@@ -2,11 +2,11 @@
 
 prefix="../../libs"
 
-for rawfile in $(find ${prefix} -name "*.idr"); do
-    file=$(echo $rawfile | sed "s|\.\./\.\./libs/\(.*\)|\1|")
-    libname=$(echo $file | sed "s|\([^/]*\)/.*|\1|")
-    filename=$(echo $file | sed "s|[^/]*/\(.*\)\.idr|\1|")
-    directories=$(echo $file | sed "s|\(.*\)/[^/]*\.idr|\1|")
+for rawfile in $(find "$prefix" -name "*.idr"); do
+    file=$(echo "$rawfile" | sed "s|\.\./\.\./libs/\(.*\)|\1|")
+    libname=$(echo "$file" | sed "s|\([^/]*\)/.*|\1|")
+    filename=$(echo "$file" | sed "s|[^/]*/\(.*\)\.idr|\1|")
+    directories=$(echo "$file" | sed "s|\(.*\)/[^/]*\.idr|\1|")
     mkdir -p "html/${directories}"
-    ${HOME}/.local/bin/katla html $rawfile "${prefix}/${libname}/build/ttc/${filename}.ttm" > "html/${libname}/${filename}.html"
+    katla html "$rawfile" "${prefix}/${libname}/build/ttc/${filename}.ttm" > "html/${libname}/${filename}.html"
 done
