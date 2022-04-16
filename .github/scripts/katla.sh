eb7245b5e7fd8d4bd5014821ed0b65978bf90b20
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-04-16 13:14:10 +0100

    lint

--- .github/scripts/katla.sh
+++ .github/scripts/katla.sh
@@ -2,14 +2,13 @@
 
 prefix="../../libs"
 
-find "$prefix" -name "*.idr" > tmp
-while IFS= read -r rawfile
-do
+find "$prefix" -name "*.idr" >tmp
+while IFS= read -r rawfile; do
     file=$(echo "$rawfile" | sed "s|\.\./\.\./libs/\(.*\)|\1|")
     libname=$(echo "$file" | sed "s|\([^/]*\)/.*|\1|")
     filename=$(echo "$file" | sed "s|[^/]*/\(.*\)\.idr|\1|")
     directories=$(echo "$file" | sed "s|[^/]*/\(.*\)/[^/]*\.idr|\1|")
     mkdir -p "html/${libname}/source/${directories}"
     katla html "$rawfile" "${prefix}/${libname}/build/ttc/${filename}.ttm" >"html/${libname}/source/${filename}.html"
-done < tmp
+done <tmp
 rm tmp
