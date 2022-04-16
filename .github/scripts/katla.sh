e6b550efc12d8f6e11043cb449b77102265256bc
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-04-16 13:01:51 +0100

    [ lint ] katla.sh + deploy libs docs too

--- .github/scripts/katla.sh
+++ .github/scripts/katla.sh
@@ -2,11 +2,14 @@
 
 prefix="../../libs"
 
-for rawfile in $(find "$prefix" -name "*.idr"); do
+find "$prefix" -name "*.idr" > tmp
+while IFS= read -r rawfile
+do
     file=$(echo "$rawfile" | sed "s|\.\./\.\./libs/\(.*\)|\1|")
     libname=$(echo "$file" | sed "s|\([^/]*\)/.*|\1|")
     filename=$(echo "$file" | sed "s|[^/]*/\(.*\)\.idr|\1|")
-    directories=$(echo "$file" | sed "s|\(.*\)/[^/]*\.idr|\1|")
-    mkdir -p "html/${directories}"
-    katla html "$rawfile" "${prefix}/${libname}/build/ttc/${filename}.ttm" > "html/${libname}/${filename}.html"
-done
+    directories=$(echo "$file" | sed "s|[^/]*/\(.*\)/[^/]*\.idr|\1|")
+    mkdir -p "html/${libname}/source/${directories}"
+    katla html "$rawfile" "${prefix}/${libname}/build/ttc/${filename}.ttm" > "html/${libname}/source/${filename}.html"
+done < tmp
+# rm tmp
