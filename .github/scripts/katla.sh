44e4dd1ff936c391f722148e43eb9793cd8c7050
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-04-16 14:54:09 +0100

    fix

--- .github/scripts/katla.sh
+++ .github/scripts/katla.sh
@@ -7,8 +7,7 @@ while IFS= read -r rawfile; do
     file=$(echo "$rawfile" | sed "s|\.\./\.\./libs/\(.*\)|\1|")
     libname=$(echo "$file" | sed "s|\([^/]*\)/.*|\1|")
     filename=$(echo "$file" | sed "s|[^/]*/\(.*\)\.idr|\1|")
-    htmlfile=$(echo "$filename" | sed "s|/|.|g")
-    directories=$(echo "$file" | sed "s|[^/]*/\(.*\)/[^/]*\.idr|\1|")
+    htmlfile=$(echo "$filename" | sed "s|/|.|g" | sed "s|\(.*\)\.idr|\1")
     mkdir -p "html/${libname}/source/"
     katla html "$rawfile" "${prefix}/${libname}/build/ttc/${filename}.ttm" >"html/${libname}/source/${htmlfile}.html"
 done <tmp
