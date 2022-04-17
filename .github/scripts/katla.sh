e0bed1f5cb05f832e7075caa3d54c462635c2029
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-04-17 11:16:28 +0100

    add links to source

--- .github/scripts/katla.sh
+++ .github/scripts/katla.sh
@@ -12,5 +12,19 @@ while IFS= read -r rawfile; do
     htmlfile="${htmldir}/${modulename}.html"
     mkdir -p "$htmldir"
     katla html "$rawfile" "${prefix}/${libname}/build/ttc/${filename}.ttm" >"$htmlfile"
+    sed -i "s|<head>|<head><title>${modulename}</title>|" "$htmlfile"
+done <tmp
+rm tmp
+
+ls "$prefix" >tmp
+while IFS=read -r libname; do
+    cp -r "$prefix"/"$libname"/build/docs/* html/"$libname"
+    find html/"$libname"/docs/ -name "*.html" >tmp2
+    while IFS=read -r rawfile; do
+        file=$(echo "$rawfile" | sed "s|/docs/\(.*\)|\1|")
+        filename=$(basename "$file" ".html")
+        sed -i "s|<h1>${filename}</h1>|<h1><a href=\"../source/${filename}.html\">${filename}</a></h1>" "$file"
+    done <tmp2
+    rm tmp2
 done <tmp
 rm tmp
