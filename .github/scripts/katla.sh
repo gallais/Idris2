7cce1ba5f41f0bb175e3fb3bc8a8532acb08a01b
Guillaume Allais <guillaume.allais@ens-lyon.org>
2022-04-19 10:39:08 +0100

    [ done ]

--- .github/scripts/katla.sh
+++ .github/scripts/katla.sh
@@ -8,7 +8,7 @@ while IFS= read -r rawfile; do
     libname=$(echo "$file" | sed "s|\([^/]*\)/.*|\1|")
     filename=$(echo "$file" | sed "s|[^/]*/\(.*\)\.idr|\1|")
     modulename=$(echo "$filename" | sed "s|/|.|g")
-    htmldir="html/${libname}/docs/source/"
+    htmldir="html/${libname}/source/"
     htmlfile="${htmldir}/${modulename}.html"
     mkdir -p "$htmldir"
     katla html "$rawfile" "${prefix}/${libname}/build/ttc/${filename}.ttm" >"$htmlfile"
@@ -16,13 +16,15 @@ while IFS= read -r rawfile; do
 done <tmp
 rm tmp
 
-for libname in "$prefix"/*; do
+find "$prefix"/* -maxdepth 0 -type d >tmp
+while IFS= read -r libdirectory; do
+    libname=$(echo "$libdirectory" | sed "s|$prefix/||")
     cp -r "$prefix"/"$libname"/build/docs/* html/"$libname"
-    find html/"$libname"/docs/ -name "*.html" >tmp
-    while IFS= read -r rawfile; do
-        file=$(echo "$rawfile" | sed "s|/docs/\(.*\)|\1|")
+    find html/"$libname"/docs/ -name "*.html" >tmp2
+    while IFS= read -r file; do
         filename=$(basename "$file" ".html")
-        sed -i "s|<h1>${filename}</h1>|<h1><a href=\"../source/${filename}.html\">${filename}</a></h1>" "$file"
-    done <tmp
-    rm tmp
-done
+        sed -i "s|<h1>${filename}</h1>|<h1><a href=\"../source/${filename}.html\">${filename}</a></h1>|" "$file"
+    done <tmp2
+    rm tmp2
+done <tmp
+rm tmp
